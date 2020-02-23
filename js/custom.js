f83b3c464a5273b7fcb96f854885bcc7306b4845
Reza <mrreza1394@gmail.com>
2020-02-23 20:35:33 +0330

    change card slider

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.card-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ],
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
